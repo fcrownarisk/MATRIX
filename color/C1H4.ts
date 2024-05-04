f358ce02be31d014576bb2d23205c25639628f87
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-05-04 17:51:19 +0800

    Update C1H4.ts

--- color/C1H4.ts
+++ color/C1H4.ts
@@ -1,19 +1,16 @@
-
-
 interface CH4 {
 carbonCount: number;
 hydrogenCount: number;
- }
- class C1H4 implements CH4 {
+}
+class C1H4 implements CH4 {
 readonly carbonCount: number = 1
 readonly hydrogenCount: number = 4
  }
-
- class C5H8 implements CH4 {
+class C5H8 implements CH4 {
 readonly carbonCount: number = 5
 readonly hydrogenCount: number = 8
  }
- class C13H24 implements CH4 {
+class C13H24 implements CH4 {
 readonly carbonCount: number = 13
 readonly hydrogenCount: number = 24
  }
